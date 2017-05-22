2b6d77a7d5b940882ff2fdf001e7127772096618
Rafael Leal <rafaelealdias@gmail.com>
2017-05-22 01:18:17 -0300

    Refactoring and fixing remove

--- listmodule/listmodule.c
+++ listmodule/listmodule.c
@@ -26,7 +26,6 @@ struct Item itemList;
 struct list_head hatefulList = LIST_HEAD_INIT(hatefulList);
 
 int getSize() {
-    struct Item *aItem;
     struct list_head *l;
     int size = 0;
     list_for_each(l, &hatefulList) {
@@ -52,17 +51,17 @@ int remove() {
     if (list_empty(&hatefulList)) {
         return -1;
     } else {
-        //struct Item firstItem = list_first_entry(&hatefulList, Item, list);
+        struct Item *firstItem = list_first_entry(&hatefulList, struct Item, list);
         //kfree(nextItem);
         list_del(hatefulList.next);
-        return 0;
+        return firstItem->value;
     }
 }
 
 int __init init_module(void) {
-  printk(KERN_INFO "Initialize InsereModule");
+  printk(KERN_INFO "Initialize ListModule");
   return 0;
 }
 void __exit cleanup_module(void) {
-  printk(KERN_INFO "Cleanup InsereModule");
+  printk(KERN_INFO "Cleanup ListModule");
 }
